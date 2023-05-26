a9c8e3821453fc6bcd7cc71cded79b65cff397fe
aayushxrj <aayush.ranjan0904@gmail.com>
2023-05-27 02:54:02 +0530

    used layouts EJS & /work & /about added

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // var items = ["Buy Food", "Cook Food", "Eat Food"];
 let items = [];
+let workItems = [];
 
 app.get('/', (req, res) => {
 
@@ -26,18 +27,37 @@ app.get('/', (req, res) => {
     let day = today.toLocaleDateString("en-US", options);
      
 
-    res.render("list", { kindOfDay: day,
+    res.render("list", { listTitle: day,
                          newListItems: items });
 });
 
 app.post("/", function(req, res){
     let item = req.body.newItem;
-    // console.log(item);
-
-    // res.render // add to one render
-    items.push(item);
-    res.redirect("/");
+    if( req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    // console.log(req.body);
 })
+
+app.get("/work", function (req, res){
+    res.render("list", {listTitle : "Work List",
+                        newListItems : workItems})
+})
+
+// app.post("/work", function(req, res){
+//     let item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect("/work");
+// })
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
 
 app.listen(3000, () => {
     console.log('Server started on port 3000...');
