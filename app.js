e8c7faa039f952145b6238cf8c7cd0850fd4ed72
aayushxrj <aayush.ranjan0904@gmail.com>
2023-05-26 19:39:43 +0530

    css added

--- app.js
+++ app.js
@@ -2,26 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+// var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = [];
 
 app.get('/', (req, res) => {
 
-    var today = new Date();
+    let today = new Date();
 
     //displaying day
     // var currentDay = today.getDay();
     // var day = dayName(currentDay);;
-    var options = {
+    let options = {
         weekday: "long",
         day : "numeric",
         month:"long"
     };
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
      
 
     res.render("list", { kindOfDay: day,
@@ -29,10 +31,10 @@ app.get('/', (req, res) => {
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     // console.log(item);
 
-    // res.refer // add to one render
+    // res.render // add to one render
     items.push(item);
     res.redirect("/");
 })
